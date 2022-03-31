f3cc9d98fb2b67b3aa1a38c3061a0296c881fd36
fifthless <102770319+fifthless@users.noreply.github.com>
2022-04-01 07:32:08 +0800

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,8 +1,8 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
+	name: "The School Tree",
+	id: "scholars",
 	author: "nobody",
-	pointsName: "points",
+	pointsName: "knowledge",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
@@ -13,14 +13,13 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.1",
+	name: "Kindergarden",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
-	<h3>v0.0</h3><br>
-		- Added things.<br>
-		- Added stuff.`
+	<h3>v0.1</h3><br>
+		- Added things to make it playable.
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
